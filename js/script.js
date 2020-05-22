e1c4072dc6d4d25c0cebddfd68235dd31eec6609
Alexei Sliusarev <alexei.sliusarev@gmail.com>
2020-05-22 13:03:09 +0300

    refactored CSS & HTML + JS toggle

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 
+function toggleNotifications() {
+    var button = document.getElementById("widget__notifications");
+    button.classList.toggle("nav__widget_active");
+  }
+
+function toggleOther() {
+    var button = document.getElementById("widget__other");
+    button.classList.toggle("nav__widget_active");
+}
