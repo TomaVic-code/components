bd19da9554be21c67b79bf8207ca6f7e840bbcf0
Toma Victor <vtomaengineering@gmail.com>
2020-05-22 20:07:21 +0300

    uploan side_notification

--- js/script.js
+++ js/script.js
@@ -1,10 +1,24 @@
 
 function toggleNotifications() {
     var button = document.getElementById("widget__notifications");
-    button.classList.toggle("nav__widget_active");
+    button.classList.toggle("nav__main__widget_active");
   }
 
 function toggleOther() {
     var button = document.getElementById("widget__other");
-    button.classList.toggle("nav__widget_active");
+    button.classList.toggle("nav__main__widget_active");
 }
+
+let side_not = document.querySelector('.nav__main__widget');
+    side_not.addEventListener('click', function(){
+  let side_appear = document.querySelector('.nav__side__notification');
+      side_appear.classList.toggle('nav__side__notification-active');
+});
+
+let closeClick = document.querySelector('.click-close');
+    closeClick.addEventListener('click', function(){
+  let side_appear = document.querySelector('.nav__side__notification');
+      side_appear.classList.remove('nav__side__notification-active');
+      var button = document.getElementById("widget__notifications");
+          button.classList.remove("nav__main__widget_active");
+});
